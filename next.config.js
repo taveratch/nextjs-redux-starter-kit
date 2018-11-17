9732bd822bb44dc05585e7ef701f977933d43757
Taweerat Chaiman <taweesoft@gmail.com>
2018-11-17 15:06:14 +0700

    remove unused code

--- next.config.js
+++ next.config.js
@@ -1,23 +1,6 @@
-// module.exports = {
-//   webpack(config, options) {
-//     const { dev, isServer } = options
-//     const extractCSSPlugin = new ExtractTextPlugin({
-//       filename: 'static/style.css',
-//       disable: dev
-//     })
-//     config.module.rules.push({
-//       test: /\.css$/,
-//       use: cssLoaderConfig(extractCSSPlugin, {
-//         cssModules,
-//         dev,
-//         isServer
-//       })
-//     })
-//     return config
-//   }
-// }
-
-const withCSS = require('@zeit/next-css')
 /* Without CSS Modules, with PostCSS */
+const withCSS = require('@zeit/next-css')
+
 const withTypescript = require('@zeit/next-typescript')
+
 module.exports = withTypescript(withCSS())
